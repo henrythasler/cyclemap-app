28d126a950962c2fcc8c4b84365fd7f42db284d8
Henry Thasler <git@thasler.com>
2020-08-20 09:04:38 +0200

    Demo Application

---
+++ app/src/main/java/com/henrythasler/cyclemap/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.henrythasler.cyclemap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
+
+class MainActivity : AppCompatActivity() {
+    private var mapView: MapView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+        setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { mapboxMap ->
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    /*
+    override fun onSaveInstanceState(outState: Bundle?) {
+        if (outState != null) {
+            super.onSaveInstanceState(outState)
+            mapbox.onSaveInstanceState(outState)
+        }
+    }
+*/
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+}
