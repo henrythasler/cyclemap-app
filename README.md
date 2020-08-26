46b3e2e573c4002355fbb05e41b7819f9c783bf8
Henry Thasler <git@thasler.com>
2020-08-26 23:44:41 +0200

    annotation test

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cyclemap-app
 android app for offline usage of vector-tiles
+
+## References
+
+### Mapbox
+
+- [Mapbox Tutorial For Android: Getting Started](https://www.raywenderlich.com/378151-mapbox-tutorial-for-android-getting-started)
--- app/src/main/java/com/henrythasler/cyclemap/MainActivity.kt
+++ app/src/main/java/com/henrythasler/cyclemap/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.henrythasler.cyclemap
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.*
 import com.mapbox.pluginscalebar.ScaleBarOptions
 import com.mapbox.pluginscalebar.ScaleBarPlugin
 
+
 class MainActivity : AppCompatActivity() {
     private var mapView: MapView? = null
+    private var map: MapboxMap? = null
+    private var symbolManager: SymbolManager? = null
+    private var symbol: Symbol? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,20 +29,76 @@ class MainActivity : AppCompatActivity() {
         mapView?.onCreate(savedInstanceState)
 
         mapView?.getMapAsync { mapboxMap ->
-            mapboxMap.setStyle(
-                Style.Builder().fromUri("asset://cyclemap-style.json")) {
-                val scaleBarPlugin = ScaleBarPlugin(mapView!!, mapboxMap)
+            map = mapboxMap
 
-                // Set the custom styling via a ScaleBarOptions object
+            map?.setStyle(
+                Style.Builder().fromUri("asset://cyclemap-style.json")
+            ) { style ->
+
+// create symbol manager object
+                symbolManager = SymbolManager(mapView!!, mapboxMap, style)
+                symbolManager?.iconAllowOverlap = true
+                symbolManager?.iconIgnorePlacement = true
+
+                symbolManager!!.addClickListener { symbol ->
+                    Toast.makeText(
+                        this,
+                        String.format("latlng=(%.2f, %.2f)", symbol.latLng.latitude, symbol.latLng.longitude),
+                        Toast.LENGTH_LONG
+                    ).show()
+                    true
+                }
+
+                symbolManager!!.addLongClickListener {symbol ->
+                    symbolManager?.delete(symbol)
+                    Toast.makeText(
+                        this,
+                        "delete marker",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    true
+                }
+
+
+                val scaleBarPlugin = ScaleBarPlugin(mapView!!, mapboxMap)
                 val scaleBarOptions = ScaleBarOptions(this)
                 scaleBarOptions
                     .setMetricUnit(true)
                     .setBarHeight(15f)
                     .setTextSize(40f)
-
-                // Create a ScaleBarOptions object to use the Plugin's default styling
                 scaleBarPlugin.create(scaleBarOptions)
+                val uiSettings = mapboxMap.uiSettings
+                uiSettings.isRotateGesturesEnabled = false
             }
+
+
+            mapboxMap.addOnMapLongClickListener { point ->
+//                Toast.makeText(
+//                    this,
+//                    String.format("User clicked long at: %s", point.toString()),
+//                    Toast.LENGTH_LONG
+//                ).show()
+
+                symbolManager!!.deleteAll()
+
+                val symbolOptions = SymbolOptions()
+                    .withLatLng(point)
+                    .withIconImage("star.white")
+                    .withIconSize(2.0f)
+                    .withSymbolSortKey(10.0f)
+                    .withDraggable(true)
+                symbol = symbolManager!!.create(symbolOptions)
+
+                true
+            }
+
+//            mapboxMap.addOnMapClickListener { point ->
+//                Toast.makeText(this, String.format("User clicked at: %s", point.toString()), Toast.LENGTH_LONG).show()
+//
+//                true
+//            }
+
         }
     }
 
