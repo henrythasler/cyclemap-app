238501b4f662bbb1722a4565ad50305d19baf7a4
Henry Thasler <git@thasler.org>
2020-09-12 14:43:16 +0200

    add location tracking

--- app/src/main/java/com/henrythasler/cyclemap/MainActivity.kt
+++ app/src/main/java/com/henrythasler/cyclemap/MainActivity.kt
@@ -4,35 +4,45 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.location.OnCameraTrackingChangedListener;
+import com.mapbox.mapboxsdk.location.OnLocationClickListener;
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.offline.OfflineManager
-import com.mapbox.mapboxsdk.plugins.annotation.Symbol
-import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
-import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete
 import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions
 import com.mapbox.pluginscalebar.ScaleBarOptions
 import com.mapbox.pluginscalebar.ScaleBarPlugin
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, Style.OnStyleLoaded, PermissionsListener, OnCameraTrackingChangedListener, OnLocationClickListener {
     private var mapView: MapView? = null
-    private var map: MapboxMap? = null
-    private var symbolManager: SymbolManager? = null
-    private var symbol: Symbol? = null
-    private val REQUEST_CODE_AUTOCOMPLETE: Int = 1
+    private lateinit var map: MapboxMap
+    private lateinit var style: Style
     private lateinit var mDrawerLayout: DrawerLayout
+    private lateinit var permissionsManager: PermissionsManager
+    private var isInTrackingMode: Boolean = false
+
+    private val REQUEST_CODE_AUTOCOMPLETE: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -120,91 +130,7 @@ class MainActivity : AppCompatActivity() {
                 }
             })
 
-
-        mapView?.getMapAsync { mapboxMap ->
-            map = mapboxMap
-            map?.setStyle(
-                Style.Builder().fromUri("https://www.cyclemap.link/cyclemap-style.json")
-            ) { style ->
-
-                // create symbol manager object
-                symbolManager = SymbolManager(mapView!!, mapboxMap, style)
-                symbolManager?.iconAllowOverlap = true
-                symbolManager?.iconIgnorePlacement = true
-
-                symbolManager!!.addClickListener { symbol ->
-                    Toast.makeText(
-                        this,
-                        String.format(
-                            "latlng=(%.2f, %.2f)",
-                            symbol.latLng.latitude,
-                            symbol.latLng.longitude
-                        ),
-                        Toast.LENGTH_LONG
-                    ).show()
-                    true
-                }
-
-                symbolManager!!.addLongClickListener { symbol ->
-                    symbolManager?.delete(symbol)
-                    Toast.makeText(
-                        this,
-                        "delete marker",
-                        Toast.LENGTH_LONG
-                    ).show()
-
-                    true
-                }
-
-
-                val scaleBarPlugin = ScaleBarPlugin(mapView!!, mapboxMap)
-                val scaleBarOptions = ScaleBarOptions(this)
-                scaleBarOptions
-                    .setMetricUnit(true)
-                    .setBarHeight(15f)
-                    .setTextSize(40f)
-                scaleBarPlugin.create(scaleBarOptions)
-                val uiSettings = mapboxMap.uiSettings
-                uiSettings.isRotateGesturesEnabled = false
-            }
-
-
-            mapboxMap.addOnMapLongClickListener { point ->
-//                Toast.makeText(
-//                    this,
-//                    String.format("User clicked long at: %s", point.toString()),
-//                    Toast.LENGTH_LONG
-//                ).show()
-
-                symbolManager!!.deleteAll()
-
-//                val symbolOptions = SymbolOptions()
-//                    .withLatLng(point)
-//                    .withIconImage("star.white")
-//                    .withIconSize(2.0f)
-//                    .withSymbolSortKey(10.0f)
-//                    .withDraggable(true)
-//                symbol = symbolManager!!.create(symbolOptions)
-
-                true
-            }
-
-            mapboxMap.addOnMapClickListener { point ->
-//                Toast.makeText(this, String.format("User clicked at: %s", point.toString()), Toast.LENGTH_LONG).show()
-
-                val placeOptions: PlaceOptions = PlaceOptions.builder()
-                    .backgroundColor(Color.parseColor("#e0ffffff"))
-                    .proximity(Point.fromLngLat(point.latitude, point.longitude))
-                    .build()
-                val intent = PlaceAutocomplete.IntentBuilder()
-                    .accessToken(getString(R.string.mapbox_access_token))
-                    .placeOptions(placeOptions)
-                    .build(this)
-                startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE)
-
-                true
-            }
-        }
+        mapView?.getMapAsync(this)
     }
 
     override fun onStart() {
@@ -246,7 +172,7 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
             val feature = PlaceAutocomplete.getPlace(data)
-            map?.animateCamera(
+            map.animateCamera(
                 CameraUpdateFactory.newCameraPosition(
                     CameraPosition.Builder()
                         .target(
@@ -259,6 +185,164 @@ class MainActivity : AppCompatActivity() {
                         .build()
                 ), 4000
             )
+        }
+    }
+
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        map = mapboxMap
+
+        map.setStyle(
+            Style.Builder().fromUri("https://www.cyclemap.link/cyclemap-style.json")
+        ) { style -> onStyleLoaded(style)}
+
+//        map.addOnMapClickListener { point ->
+//            val placeOptions: PlaceOptions = PlaceOptions.builder()
+//                .backgroundColor(Color.parseColor("#e0ffffff"))
+//                .proximity(Point.fromLngLat(point.latitude, point.longitude))
+//                .build()
+//            val intent = PlaceAutocomplete.IntentBuilder()
+//                .accessToken(getString(R.string.mapbox_access_token))
+//                .placeOptions(placeOptions)
+//                .build(this)
+//            startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE)
+//
+//            true
+//        }
+    }
+
+    override fun onStyleLoaded(mapboxStyle: Style) {
+        style = mapboxStyle
+
+        val scaleBarPlugin = ScaleBarPlugin(mapView!!, map)
+        val scaleBarOptions = ScaleBarOptions(this)
+        scaleBarOptions
+            .setMetricUnit(true)
+            .setBarHeight(15f)
+            .setTextSize(40f)
+        scaleBarPlugin.create(scaleBarOptions)
+        val uiSettings = map.uiSettings
+        uiSettings.isRotateGesturesEnabled = false
+
+        Toast.makeText(
+            this,
+            "onStyleLoaded()",
+            Toast.LENGTH_LONG
+        ).show()
+
+        enableLocationComponent()
+
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private fun enableLocationComponent() {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+            Toast.makeText(
+                this,
+                "Location permissions granted",
+                Toast.LENGTH_LONG
+            ).show()
+
+            // Get an instance of the component
+            val locationComponent = map.locationComponent
+
+            val locationComponentOptions = LocationComponentOptions.builder(this)
+                .pulseEnabled(true)
+                .trackingGesturesManagement(true)
+                .pulseColor(Color.parseColor("#00c000"))
+//                .pulseAlpha(.4f)
+//                .pulseInterpolator(BounceInterpolator())
+                .build()
+
+            val locationComponentActivationOptions = LocationComponentActivationOptions
+                .builder(this, style)
+                .locationComponentOptions(locationComponentOptions)
+                .useDefaultLocationEngine(true)
+                .build()
+
+            // Activate with a built LocationComponentActivationOptions object
+            locationComponent?.activateLocationComponent(locationComponentActivationOptions)
+
+            // Enable to make component visible
+            locationComponent?.isLocationComponentEnabled = true
+
+            // Set the component's camera mode
+            locationComponent?.cameraMode = CameraMode.TRACKING
+
+            // Set the component's render mode
+            locationComponent?.renderMode = RenderMode.COMPASS
+
+            // Add the location icon click listener
+            locationComponent.addOnLocationClickListener(this);
+
+            // Add the camera tracking listener. Fires if the map camera is manually moved.
+            locationComponent.addOnCameraTrackingChangedListener(this);
+
+            findViewById<FloatingActionButton>(R.id.back_to_camera_tracking_mode).setOnClickListener {
+                if (!isInTrackingMode) {
+                    isInTrackingMode = true;
+                    locationComponent.cameraMode = CameraMode.TRACKING;
+                    Toast.makeText(
+                        this, getString(R.string.tracking_enabled),
+                        Toast.LENGTH_SHORT
+                    ).show();
+                } else {
+                    Toast.makeText(
+                        this, getString(R.string.tracking_already_enabled),
+                        Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+
+        } else {
+            Toast.makeText(
+                this,
+                "Requesting Location permissions",
+                Toast.LENGTH_LONG
+            ).show()
+            permissionsManager = PermissionsManager(this)
+            permissionsManager?.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(
+            this,
+            "onExplanationNeeded()",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        Toast.makeText(
+            this,
+            "onPermissionResult()",
+            Toast.LENGTH_LONG
+        ).show()
+
+        if(granted) {
+            enableLocationComponent()
+        }
+    }
+
+    override fun onCameraTrackingChanged(currentMode: Int) {
+    }
+
+    override fun onCameraTrackingDismissed() {
+        isInTrackingMode = false;
+    }
+
+    override fun onLocationComponentClick() {
+        // Get an instance of the component
+        val locationComponent = map.locationComponent
+        if(locationComponent.lastKnownLocation != null) {
+            Toast.makeText(
+                this,
+                String.format(getString(R.string.current_location), locationComponent.lastKnownLocation?.latitude, locationComponent.lastKnownLocation?.longitude),
+                Toast.LENGTH_LONG
+            ).show();
         }
     }
 }
