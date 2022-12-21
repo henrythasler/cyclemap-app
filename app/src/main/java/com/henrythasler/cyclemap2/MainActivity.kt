7568e76bce71f5b211f3d9bd56f5cc0739fed3a6
Henry Thasler <git@thasler.org>
2022-12-21 11:32:43 +0100

    clean up

--- app/src/main/java/com/henrythasler/cyclemap2/MainActivity.kt
+++ app/src/main/java/com/henrythasler/cyclemap2/MainActivity.kt
@@ -1,17 +1,7 @@
 package com.henrythasler.cyclemap2
 
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.henrythasler.cyclemap2.databinding.ActivityMainBinding
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
