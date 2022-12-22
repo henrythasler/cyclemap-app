ca88a7a54ee8b77eb6b5d9bfdd0cf816e11dfdbf
Henry Thasler <git@thasler.org>
2022-12-22 14:57:19 +0100

    save/restore camera settings

--- app/src/main/java/com/henrythasler/cyclemap2/MainActivity.kt
+++ app/src/main/java/com/henrythasler/cyclemap2/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.henrythasler.cyclemap2
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.mapbox.android.gestures.MoveGestureDetector
+import com.mapbox.geojson.Point
 import com.mapbox.maps.*
 import com.mapbox.maps.extension.style.layers.Layer
 import com.mapbox.maps.extension.style.layers.getLayer
 import com.mapbox.maps.extension.style.layers.properties.generated.Visibility
 import com.mapbox.maps.plugin.animation.MapAnimationOptions
+import com.mapbox.maps.plugin.animation.camera
 import com.mapbox.maps.plugin.animation.easeTo
+import com.mapbox.maps.plugin.gestures.GesturesPlugin
+import com.mapbox.maps.plugin.gestures.OnMoveListener
 import com.mapbox.maps.plugin.gestures.gestures
 import com.mapbox.maps.plugin.locationcomponent.location
 
@@ -22,19 +29,41 @@ var map: MapboxMap? = null
 
 class MainActivity : AppCompatActivity() {
 
+    private val moveListener: OnMoveListener = object : OnMoveListener {
+        override fun onMoveBegin(detector: MoveGestureDetector) {
+        }
+
+        override fun onMove(detector: MoveGestureDetector): Boolean {
+            return false
+        }
+
+        override fun onMoveEnd(detector: MoveGestureDetector) {
+
+            // this is a workaround to reliably set the focalPoint for double-tap-zoom
+            mapView?.gestures?.focalPoint = map?.pixelForCoordinate(map!!.cameraState.center)
+            Log.i("App", "onMoveEnd(): focalPoint=" + mapView?.gestures?.focalPoint.toString())
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
         map = mapView?.getMapboxMap()
 
+        restoreSettings()
+
         map?.loadStyleUri(resources.getString(R.string.MAIN_STYLE_URL)) {
                 style -> onStyleLoaded(style)
         }
 
+        // set up user interaction
         val locateButton: View = findViewById(R.id.locate)
         locateButton.setOnClickListener { onLocateButton()}
 
+        mapView?.gestures?.addOnMoveListener(moveListener)
+
+        // set up drawer menu
         val mDrawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navigationView: NavigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener { menuItem ->
@@ -43,8 +72,11 @@ class MainActivity : AppCompatActivity() {
 
             when (menuItem.itemId) {
                 R.id.nav_hillshading -> {
-                    val layer: Layer? = mapView?.getMapboxMap()?.getStyle()?.getLayer("hillshading");
-                    layer?.visibility( if (layer?.visibility == Visibility.NONE) Visibility.VISIBLE else Visibility.NONE );
+                    mapView?.getMapboxMap()?.getStyle() {
+                        it.getLayer("hillshading")?.let { layer ->
+                            layer.visibility( if(layer.visibility == Visibility.NONE) Visibility.VISIBLE else Visibility.NONE)
+                        }
+                    }
                 }
                 R.id.nav_shareposition -> {
                     Toast.makeText(this, "nav_shareposition", Toast.LENGTH_LONG).show()
@@ -63,22 +95,41 @@ class MainActivity : AppCompatActivity() {
  */
     }
 
-    private fun onStyleLoaded(style: Style) {
-        val cameraPosition = CameraOptions.Builder()
-            .zoom(12.0)
-            .build()
-        val mapAnimationOptions = MapAnimationOptions.Builder().duration(1000).build()
-        mapView?.getMapboxMap()?.easeTo(cameraPosition, mapAnimationOptions)
-        // mapView?.getMapboxMap()?.setCamera(cameraPosition)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i("App", "onSaveInstanceState()")
 
+        // see https://developer.android.com/training/data-storage/shared-preferences
+        with(getPreferences(Context.MODE_PRIVATE).edit()) {
+            putFloat(getString(R.string.STATE_LATITUDE), map!!.cameraState.center.latitude().toFloat())
+            putFloat(getString(R.string.STATE_LONGITUDE), map!!.cameraState.center.longitude().toFloat())
+            putFloat(getString(R.string.STATE_ZOOM), map!!.cameraState.zoom.toFloat())
+            commit()
+        }
+    }
+
+    private fun restoreSettings() {
+        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+        val cameraPosition = CameraOptions.Builder()
+            .zoom(sharedPref.getFloat(getString(R.string.STATE_ZOOM), getString(R.string.DEFAULT_ZOOM).toFloat()).toDouble())
+            .center(Point.fromLngLat(
+                sharedPref.getFloat(getString(R.string.STATE_LONGITUDE), getString(R.string.DEFAULT_LONGITUDE).toFloat()).toDouble(),
+                sharedPref.getFloat(getString(R.string.STATE_LATITUDE), getString(R.string.DEFAULT_LATITUDE).toFloat()).toDouble()
+            ))
+            .build()
+        Log.i("App", "restoreSettings(): center=" + cameraPosition.center?.coordinates().toString() + " zoom=" + cameraPosition.zoom.toString())
+        // set camera position
+        map?.setCamera(cameraPosition)
+    }
+
+    private fun onStyleLoaded(style: Style) {
         // prevent certain UI operations
         mapView?.gestures?.rotateEnabled = false
         mapView?.gestures?.pitchEnabled = false
 
-        Toast.makeText(this, map?.pixelForCoordinate(map!!.cameraState.center).toString(), Toast.LENGTH_LONG).show()
-
         // make sure we always zoom into the map center
         mapView?.gestures?.focalPoint = map?.pixelForCoordinate(map!!.cameraState.center)
+        Log.i("App", "onStyleLoaded(): focalPoint=" + mapView?.gestures?.focalPoint.toString())
     }
 
     private fun onLocateButton() {
