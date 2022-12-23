68adf629231cb7f52aea85b8f8453414f044f96f
Henry Thasler <git@thasler.org>
2022-12-23 15:22:23 +0100

    UI refactor

--- app/src/main/java/com/henrythasler/cyclemap2/MainMapActivity.kt
+++ app/src/main/java/com/henrythasler/cyclemap2/MainMapActivity.kt
@@ -1,27 +1,23 @@
 package com.henrythasler.cyclemap2
 
 import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
 import com.mapbox.android.gestures.MoveGestureDetector
 import com.mapbox.geojson.Point
-import com.mapbox.maps.*
-import com.mapbox.maps.extension.style.layers.getLayer
-import com.mapbox.maps.extension.style.layers.properties.generated.Visibility
-
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
+import com.mapbox.maps.Style
 import com.mapbox.maps.plugin.gestures.OnMoveListener
 import com.mapbox.maps.plugin.gestures.gestures
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
 import com.mapbox.maps.plugin.locationcomponent.location2
 import java.lang.ref.WeakReference
 
-
-class MainActivity : AppCompatActivity() {
+class MainMapActivity : AppCompatActivity() {
     private lateinit var locationPermissionHelper: LocationPermissionHelper
     private lateinit var map: MapboxMap
     private lateinit var mapView: MapView
@@ -45,20 +41,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val onIndicatorPositionChangedListener = OnIndicatorPositionChangedListener {
-        if(trackingEnabled)
+        if (trackingEnabled)
             mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(it).build())
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_map)
         mapView = findViewById(R.id.mapView)
         map = mapView.getMapboxMap()
 
         restoreSettings()
 
-        map.loadStyleUri(resources.getString(R.string.MAIN_STYLE_URL)) {
-                style -> onStyleLoaded(style)
+        map.loadStyleUri(resources.getString(R.string.MAIN_STYLE_URL)) { style ->
+            onStyleLoaded(style)
         }
 
         // location tracking
@@ -68,13 +64,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         // set up user interaction
-        val locateButton: View = findViewById(R.id.locate)
-        locateButton.setOnClickListener { onLocateButton()}
+        val locateButton: View = findViewById(R.id.imageButton3)
+        locateButton.setOnClickListener { view -> onLocateButton(view) }
 
         mapView.gestures.addOnMoveListener(moveListener)
         mapView.location2.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
 
         // set up drawer menu
+/*
         val mDrawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navigationView: NavigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener { menuItem ->
@@ -95,7 +92,7 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
-
+*/
 /*
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
@@ -121,8 +118,14 @@ class MainActivity : AppCompatActivity() {
 
         // see https://developer.android.com/training/data-storage/shared-preferences
         with(getPreferences(Context.MODE_PRIVATE).edit()) {
-            putFloat(getString(R.string.STATE_LATITUDE), map.cameraState.center.latitude().toFloat())
-            putFloat(getString(R.string.STATE_LONGITUDE), map.cameraState.center.longitude().toFloat())
+            putFloat(
+                getString(R.string.STATE_LATITUDE),
+                map.cameraState.center.latitude().toFloat()
+            )
+            putFloat(
+                getString(R.string.STATE_LONGITUDE),
+                map.cameraState.center.longitude().toFloat()
+            )
             putFloat(getString(R.string.STATE_ZOOM), map.cameraState.zoom.toFloat())
             commit()
         }
@@ -131,13 +134,30 @@ class MainActivity : AppCompatActivity() {
     private fun restoreSettings() {
         val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
         val cameraPosition = CameraOptions.Builder()
-            .zoom(sharedPref.getFloat(getString(R.string.STATE_ZOOM), getString(R.string.DEFAULT_ZOOM).toFloat()).toDouble())
-            .center(Point.fromLngLat(
-                sharedPref.getFloat(getString(R.string.STATE_LONGITUDE), getString(R.string.DEFAULT_LONGITUDE).toFloat()).toDouble(),
-                sharedPref.getFloat(getString(R.string.STATE_LATITUDE), getString(R.string.DEFAULT_LATITUDE).toFloat()).toDouble()
-            ))
+            .zoom(
+                sharedPref.getFloat(
+                    getString(R.string.STATE_ZOOM),
+                    getString(R.string.DEFAULT_ZOOM).toFloat()
+                ).toDouble()
+            )
+            .center(
+                Point.fromLngLat(
+                    sharedPref.getFloat(
+                        getString(R.string.STATE_LONGITUDE),
+                        getString(R.string.DEFAULT_LONGITUDE).toFloat()
+                    ).toDouble(),
+                    sharedPref.getFloat(
+                        getString(R.string.STATE_LATITUDE),
+                        getString(R.string.DEFAULT_LATITUDE).toFloat()
+                    ).toDouble()
+                )
+            )
             .build()
-        Log.i("App", "restoreSettings(): center=" + cameraPosition.center?.coordinates().toString() + " zoom=" + cameraPosition.zoom.toString())
+        Log.i(
+            "App",
+            "restoreSettings(): center=" + cameraPosition.center?.coordinates()
+                .toString() + " zoom=" + cameraPosition.zoom.toString()
+        )
         // set camera position
         map.setCamera(cameraPosition)
     }
@@ -152,19 +172,21 @@ class MainActivity : AppCompatActivity() {
         Log.i("App", "onStyleLoaded(): focalPoint=" + mapView.gestures.focalPoint.toString())
     }
 
-    private fun onLocateButton() {
-        if(mapView.location2.enabled) {
+    private fun onLocateButton(view: View) {
+        if (mapView.location2.enabled) {
             // jump back to current location if the user has manually scrolled away
-            if(!trackingEnabled) {
+            if (!trackingEnabled) {
+                Log.i("App", "jumping back to location...")
                 trackingEnabled = true
             }
             // disable tracking if the user is currently tracking the position
             else {
+                Log.i("App", "disabling location tracking...")
                 mapView.location2.enabled = false
             }
-        }
-        else {
+        } else {
             // enable location tracking
+            Log.i("App", "enabling location tracking...")
             mapView.location2.apply {
                 enabled = true
                 pulsingEnabled = true
