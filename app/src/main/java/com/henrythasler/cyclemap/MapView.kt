0a0d69daa93d2b67b0f7e25adaae6f88a8049506
Henry <git@thasler.org>
2024-08-03 12:44:56 +0200

    add crosshair

--- app/src/main/java/com/henrythasler/cyclemap/MapView.kt
+++ app/src/main/java/com/henrythasler/cyclemap/MapView.kt
@@ -1,10 +1,18 @@
 package com.henrythasler.cyclemap
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,37 +20,47 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mapbox.geojson.Point
 import com.mapbox.maps.MapboxExperimental
 import com.mapbox.maps.Style
 import com.mapbox.maps.extension.compose.MapboxMap
-import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
+import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.mapbox.maps.extension.compose.rememberMapState
 import com.mapbox.maps.extension.compose.style.MapStyle
 
 @OptIn(MapboxExperimental::class)
-@Preview
 @Composable
 fun CyclemapView(styleUrl: String = stringResource(id = R.string.style_cyclemap_url)) {
-    val mapViewportState = remember {
-        MapViewportState().apply {
-            setCameraOptions {
-                zoom(12.0)
-                center(Point.fromLngLat(10.85, 48.05))
-                pitch(0.0)
-                bearing(0.0)
-            }
+    val mapViewportState = rememberMapViewportState() {
+        setCameraOptions {
+            zoom(12.0)
+            center(Point.fromLngLat(10.85, 48.05))
+            pitch(0.0)
+            bearing(0.0)
         }
     }
+    val mapState = rememberMapState() {
+        gesturesSettings = gesturesSettings.toBuilder()
+            .setRotateEnabled(false)
+            .setPitchEnabled(false)
+//            .setFocalPoint(ScreenCoordinate(100.0, 200.0))
+//            .focalPoint(pixelForCoordinate(mapViewportState.cameraState?.center))
+            .build()
+    }
     var useCustomStyle by remember { mutableStateOf(true) }
+
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
         MapboxMap(
             modifier = Modifier.fillMaxSize(),
             mapViewportState = mapViewportState,
+            mapState = mapState,
             style = {
                 if (useCustomStyle) {
                     MapStyle(style = styleUrl)
@@ -52,22 +70,56 @@ fun CyclemapView(styleUrl: String = stringResource(id = R.string.style_cyclemap_
             },
             scaleBar = {
                 ScaleBar(
-                    alignment = Alignment.BottomEnd,
+                    Modifier.padding(bottom = 48.dp),
+                    alignment = Alignment.BottomStart,
                     height = 5.dp,
                     borderWidth = 1.dp,
                     isMetricUnit = true,
+                    textSize = 14.sp,
                 )
             },
+        ) {
+            // do stuff if needed
+        }
+
+        Image(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .alpha(0.5f),
+            painter = painterResource(id = R.drawable.my_location_48px),
+            contentDescription = "",
         )
 
-        // A button to toggle between styles
-        Button(
+        val padding = 8.dp
+        Column(
             modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 64.dp),
-            onClick = { useCustomStyle = !useCustomStyle },
+                .padding(padding)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center
         ) {
-            Text("Toggle Style")
+            SmallFloatingActionButton(
+                onClick = {
+                    useCustomStyle = !useCustomStyle
+                },
+            ) {
+                Icon(Icons.Filled.Home, "Small floating action button.")
+            }
+
+            SmallFloatingActionButton(
+                onClick = {
+                    useCustomStyle = !useCustomStyle
+                },
+            ) {
+                Icon(Icons.Filled.LocationOn, "Small floating action button.")
+            }
+
+            SmallFloatingActionButton(
+                onClick = {
+                    useCustomStyle = !useCustomStyle
+                },
+            ) {
+                Icon(Icons.Filled.Add, "Small floating action button.")
+            }
         }
     }
 }
